77fe82bfc633be240926668e097c9619c387b6ba
AliRazaDeveloper75 <alirazadeveloper75@gmail.com>
2026-03-13 20:45:20 +0500

    Robust Django 4.0+ compatibility patches for django-jet

--- config/asgi.py
+++ config/asgi.py
@@ -1,6 +1,17 @@
 import os
-
 import django
+from django.utils import translation
+
+# Django 4.0+ compatibility monkey-patch
+if not hasattr(translation, 'ugettext_lazy'):
+    translation.ugettext_lazy = translation.gettext_lazy
+if not hasattr(translation, 'ugettext'):
+    translation.ugettext = translation.gettext
+if not hasattr(translation, 'ungettext_lazy'):
+    translation.ungettext_lazy = translation.ngettext_lazy
+if not hasattr(translation, 'ungettext'):
+    translation.ungettext = translation.ngettext
+
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,20 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Django 4.0+ compatibility monkey-patch
+try:
+    from django.utils import translation
+    if not hasattr(translation, 'ugettext_lazy'):
+        translation.ugettext_lazy = translation.gettext_lazy
+    if not hasattr(translation, 'ugettext'):
+        translation.ugettext = translation.gettext
+    if not hasattr(translation, 'ungettext_lazy'):
+        translation.ungettext_lazy = translation.ngettext_lazy
+    if not hasattr(translation, 'ungettext'):
+        translation.ungettext = translation.ngettext
+except ImportError:
+    pass
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
